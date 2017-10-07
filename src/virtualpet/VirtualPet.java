1a624a35482c22334043d7048b28d3feaef626dc
Sarah Strange <strange1120@gmail.com>
2017-10-07 16:47:59 -0400

    added random tick added system exit game over

--- src/virtualpet/VirtualPet.java
+++ src/virtualpet/VirtualPet.java
@@ -5,53 +5,98 @@ public class VirtualPet {
 	private int hunger;
 	private int energy;
 	private int boredom;
-	
-	
+
 	public VirtualPet(int hunger, int energy, int boredom) {
-		this.hunger = hunger; 
+		this.hunger = hunger;
 		this.energy = energy;
 		this.boredom = boredom;
 	}
-	
+
 	public int getHunger() {
 		return hunger;
 	}
+
 	public int getEnergy() {
 		return energy;
 	}
-	
+
 	public int getBoredom() {
 		return boredom;
 	}
-	
-	
+
 	public void feed(int amount) {
 		hunger += amount;
 		energy += amount;
 	}
-	
+
 	public void givePuzzle(int amount) {
 		boredom -= amount;
 		hunger -= amount;
 	}
-	
+
 	public void giveHiding(int amount) {
 		energy -= amount;
-		hunger -= amount; 
+		hunger -= amount;
 	}
+
 	public void sleep(int amount) {
 		energy += amount;
 		boredom += amount;
-}
-	public void tick(int amount) {
-		hunger -= amount;
-		energy -= amount;
-		boredom -= amount;
 	}
+
 	public String oswaldStats() {
-		return "Here are Oswalds current stats:\n"
-				+ "Hunger:" + hunger + "\nEnergy:" + energy + "\nBoredom:" + boredom;
-		
+		return "Here are Oswalds current stats:\n" + "Hunger:" + hunger + "\nEnergy:" + energy + "\nBoredom:" + boredom;
 	}
-	
+
+	public void tick(int random, int amount) {
+		if (random == 1) {
+			if (hunger < 20) {
+				hunger += amount;
+			} else if (energy < 20) {
+				energy += amount;
+			} else if (boredom > 30) {
+				boredom -= amount;
+			} else {
+				hunger += amount;
+				boredom -= amount;
+				energy -= amount;
+			}
+		}
+		if (random == 2) {
+			if (hunger < 20) {
+				hunger += amount;
+			} else if (energy < 20) {
+				energy += amount;
+			} else if (boredom > 30) {
+				boredom -= amount;
+			} else {
+				hunger -= amount;
+				boredom += amount;
+				energy -= amount;
+			}
+		}
+		if (random == 3) {
+			if (hunger < 20) {
+				hunger += amount;
+			} else if (energy < 20) {
+				energy += amount;
+			} else if (boredom > 30) {
+				boredom -= amount;
+			} else {
+				hunger -= amount;
+				boredom -= amount;
+				energy += amount;
+			}
+
+		}
+		if (hunger > 50) {
+			hunger = 50;
+		}
+		if (energy > 50) {
+			energy = 50;
+		}
+		if (boredom < 0) {
+			boredom = 0;
+		}
+	}
 }
--- src/virtualpet/VirtualPetApp.java
+++ src/virtualpet/VirtualPetApp.java
@@ -1,35 +1,46 @@
 package virtualpet;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class VirtualPetApp {
-	
+
+	public static void gameOver(int hunger, int energy, int boredom) {
+		if (hunger < 0) {
+			System.out.println("You let Oswald starve to death! Shame on you!");
+			System.exit(0);
+		}
+		if (energy < 0) {
+			System.out.println("Oswald has died of exhaustion! Shame on you!");
+			System.exit(0);
+		}
+		if (boredom > 30) {
+			System.out.println("Oswald has died of boredom! Shame on you!");
+			System.exit(0);
+		}
+	}
+
 	private static String menuList;
 
 	public static void displayMenu(String menuList) {
-		System.out.println("What would you like to do with Oswald?\n"
-				+ "1. Feed Oswald.\n"
-				+ "2. Give Oswald a puzzle to solve.\n"
-				+ "3. Give Oswald a new place to play and hide.\n"
-				+ "4. Let Oswald get some sleep.\n"
-				+ "5. Leave Oswald alone with his thoughts.\n"
-				+ "6. Pick up Oswald and try to cuddle.");
-		}
-	
-	
+		System.out.println(
+				"What would you like to do with Oswald?\n" + "1. Feed Oswald.\n" + "2. Give Oswald a puzzle to solve.\n"
+						+ "3. Give Oswald a new place to play and hide.\n" + "4. Let Oswald get some sleep.\n"
+						+ "5. Leave Oswald alone with his thoughts.\n" + "6. Pick up Oswald and try to cuddle.");
+	}
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		VirtualPet stats = new VirtualPet(50,40,45);
+		Random rand = new Random();
+		VirtualPet stats = new VirtualPet(50, 40, 5);
 
 		System.out.println("Meet Oswald the Octopus!");
-		System.out.println("Contrary to popular belief, "
-				+ "octopi are actually very intelligent and "
-				+ "capable of solving complex puzzles");		
-		
+		System.out.println("Contrary to popular belief, " + "octopi are actually very intelligent and "
+				+ "capable of solving complex puzzles");
+
 		System.out.println(stats.oswaldStats());
 		displayMenu(menuList);
-		
-		
+
 		int selection = input.nextInt();
 		while (selection < 1 || selection > 6) {
 			System.out.println("You have entered an invalid choice.");
@@ -39,51 +50,67 @@ public class VirtualPetApp {
 		}
 		int amount = 5;
 		while (selection >= 1 && selection <= 6) {
+			int random = rand.nextInt(3) + 1;
 			if (selection == 1) {
-				stats.feed(amount);
-				System.out.println("You have fed Oswald.");
+				gameOver(stats.getHunger(), stats.getEnergy(), stats.getBoredom());
+				if (stats.getHunger() == 50) {
+					System.out.println("Oswald is full. Please don't overfeed him.");
+				} else if (stats.getBoredom() > 20) {
+					System.out.println("Oswald is too bored to think about eating!");
+				} else {
+					System.out.println("You have fed Oswald.");
+					stats.feed(amount);
+				}
 			}
 			if (selection == 2) {
+				gameOver(stats.getHunger(), stats.getEnergy(), stats.getBoredom());
+				if (stats.getEnergy() < 25) {
+					System.out.println("Oswald is brain fried. Maybe you should let him sleep.");
+				} else {
 					stats.givePuzzle(amount);
 					System.out.println("Oswald is now working on solving his puzzle.");
+				}
 			}
 			if (selection == 3) {
-				stats.giveHiding(amount);
-				System.out.println("You've provided Oswald with a new hide and play spot.");
+				gameOver(stats.getHunger(), stats.getEnergy(), stats.getBoredom());
+				if (stats.getHunger() < 30) {
+					System.out.println("Oswald is hangry. He has no energy to play!");
+				} else {
+					stats.giveHiding(amount);
+					System.out.println("You've provided Oswald with a new hide and play spot.");
+				}
 			}
-			if  (selection == 4) {
+			if (selection == 4) {
+				gameOver(stats.getHunger(), stats.getEnergy(), stats.getBoredom());
 				stats.sleep(amount);
 				System.out.println("You've let Oswald get some sleep.");
 			}
-
 			if (selection == 5) {
+				gameOver(stats.getHunger(), stats.getEnergy(), stats.getBoredom());
 				System.out.println("Oswald appreciates this moment of solitude. Namaste.");
 			}
-			
 			if (selection == 6) {
-				if (stats.getHunger() >= 30 && stats.getEnergy() >= 30 && stats.getBoredom() <=30) {
+				gameOver(stats.getHunger(), stats.getEnergy(), stats.getBoredom());
+				if (stats.getHunger() >= 30 && stats.getEnergy() >= 30 && stats.getBoredom() < 30) {
 					System.out.println("Oswald appreciates that you're taking care of him properly. He loves you");
-				} else if (stats.getHunger() < 30 && stats.getEnergy() < 30 && stats.getBoredom() > 30) {
+				} else if (stats.getHunger() < 30 && stats.getEnergy() < 30 && stats.getBoredom() > 15) {
 					System.out.println("Oswald has inked you! He is not happy with the state of his care.");
-				} else  {
-					System.out.println("Oswald is trying to escape. Maybe you could improve his care.");				}
+				} else {
+					System.out.println("Oswald is trying to escape. Maybe you could improve his care.");
+				}
 			}
-			stats.tick(amount);
+			stats.tick(random, amount);
+			gameOver(stats.getHunger(), stats.getEnergy(), stats.getBoredom());
 			System.out.println(stats.oswaldStats());
 			displayMenu(menuList);
 			selection = input.nextInt();
 			while (selection < 0 || selection > 6) {
 				System.out.println("You have entered an invalid choice.");
 				System.out.println(stats.oswaldStats());
-				displayMenu(menuList);;
+				displayMenu(menuList);
 				selection = input.nextInt();
-
 			}
-
 		}
-		
 		input.close();
 	}
-		
 }
-
